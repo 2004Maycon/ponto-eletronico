e28d122617e5c57fe85c6266c81f8dd017f40505
Maycon pequeno <pequenosilvamaycon@gmail.com>
2026-06-01 02:14:57 -0300

    Update login-entrada.py

--- login-entrada/login-entrada.py
+++ login-entrada/login-entrada.py
@@ -145,7 +145,7 @@ def main(page: ft.Page):
                         txt_email,
                         txt_senha,
                        # Como tem que ficar (CORRETO):
-                        ft.Container(content=check_admin, width=340, alignment=ft.alignment.CENTER_LEFT),
+                        ft.Container(content=check_admin, width=340, alignment=ft.Alignment.CENTER_LEFT),
                         ft.Container(height=5),
                         ft.ElevatedButton("Salvar Cadastro", on_click=acao_cadastrar, bgcolor="green", color="white", width=340, height=50),
                         lbl_status,
